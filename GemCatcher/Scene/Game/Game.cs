fdd883cc3476b5d557e7e1fca9af01becb3ab468
Raymon_Rutten <80789205+Zerophreak@users.noreply.github.com>
2025-09-25 16:49:42 +0200

    Finished product

--- GemCatcher/Scene/Game/Game.cs
+++ GemCatcher/Scene/Game/Game.cs
@@ -7,11 +7,15 @@ public partial class Game : Node2D
 	const double GEM_MARGIN = 50.0;
 	[Export] private PackedScene _gemScene;
 	[Export] private Timer _spawnTimer;
+	[Export] private Label _scoreLabel;
+	[Export] private AudioStreamPlayer _music;
+	[Export] private AudioStreamPlayer2D _effects;
+	[Export] private AudioStream _explodeSound;
 	// Called when the node enters the scene tree for the first time.
+
+	private int _score = 0;
 	public override void _Ready()
 	{
-		//Gem gem = GetNode<Gem>("Gem");
-		//_gem.OnScored += OnScored;
 		_spawnTimer.Timeout += SpawnGem;
 		SpawnGem();
 	}
@@ -20,7 +24,7 @@ public partial class Game : Node2D
 	public override void _Process(double delta)
 	{
 	}
-	
+
 	private void SpawnGem()
 	{
 		Rect2 vpr = GetViewportRect();
@@ -31,12 +35,33 @@ public partial class Game : Node2D
 		float rX = (float)GD.RandRange(
 			vpr.Position.X + GEM_MARGIN, vpr.End.X - GEM_MARGIN
 		);
+
 		gem.Position = new Vector2(rX, -100);
 		gem.OnScored += OnScored;
+		gem.OnGemOffScreen += GameOver;
 	}
 
 	private void OnScored()
 	{
 		GD.Print("Onscored Received");
+		_score += 1;
+		_scoreLabel.Text = $"{_score:0000}";
+		_effects.Play();
+
+	}
+
+	private void GameOver()
+	{
+		GD.Print("GameOver");
+		foreach (Node node in GetChildren())
+		{
+			node.SetProcess(false);
+		}
+		_spawnTimer.Stop();
+		_music.Stop();
+
+		_effects.Stop();
+		_effects.Stream = _explodeSound;
+		_effects.Play();
 	}
 }
